c045528cf612ebe5b473501069478ccceaef4a9d
“Anna <annaovcharova84@gmail.com>
2022-09-07 12:55:23 -0400

    comments

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+// Importing handlers
 const{
     getAllExercises, 
     getExercisesByTarget,
@@ -21,21 +22,35 @@ const{
 express()
 .use(morgan("tiny"))
 .use(express.json())
-
+ // get all exercises USED
 .get("/exercises", getAllExercises)
+ // get exercise by id USED
 .get("/exercises/exercise/:exercise", exerciseById)
+ // get exercise by target
 .get("/exercises/target/:target", getExercisesByTarget)
+// get exercise by bodyPart
 .get("/exercises/bodyPart/:bodyPart", getExercisesByBodyPart)
+    // get exercise by name
 .get("/exercises/name/:name", getExercisesByName)
+ // get exercises by equipment type
 .get("/exercises/equipment/:equipment", getExercisesByEquipment)
+  // create a new user USED
 .post("/create-user", createNewUser)
+  // post a sign in
 .post("/signin", handleSignIn)
+  //add exercise in favorites USED
 .put("/favorites", addToFavorites)
+   //remove exercise from favorites USED
 .delete("/favorites", deleteFromFavorites)
+ // create a new workout USED
 .put("/workout", createWorkout)
+ // get a new workout by id
 .get("/workout/:userId", getWorkout)
+  //add exercise to workout USED
 .put("/addExercise", addExerciseToWorkout)
+//get user by Id
 .get("/user/:userId", getUserById)
+ // update a user USED
 .patch("/user/:userId", updateUser)
 
 
@@ -46,7 +61,7 @@ express()
         message: "This is obviously not what you are looking for.",
     });
 })
-
+ // Node spins up our server and sets it to listen on port 8000.
 .listen(8000, () => {
     console.log(`Server launched on port 8000`)
 });
